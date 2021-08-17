b18aecb550b04b4d5d8ad0720b3668395e8223bb
Uener Coelho <uenercoelho2@hotmail.com>
2021-08-17 03:12:18 -0400

    Catita Sobrancelhas :tada:

---
+++ README.md
@@ -0,0 +1,14 @@
+# Projeto CatitaSobrancelhas
+
+um projeto desenvolvido com base no OriginSix do NLW Together, Ministrado pela @rocketseat, de cunho acadêmico, ministrado pelo @maykbrito.
+
+## Tecnologias Utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+> ### Modelo de Licença
+>
+> Licença utilizada neste projeto foi o [MIT](https://github.com/rocketseat-education/maratona-discover-01/blob/main/LICENSE.md")
+# CatitaSobrancelhas
---
+++ main.js
@@ -0,0 +1,113 @@
+/* Abre e fecha o menu quando clicar nos ícones: hambúrguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* Quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* Mudar o header da pagina quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    // Se o Scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // Se o Scroll é menor que a altura do header
+    header.classList.remove('scroll')
+  }
+}
+
+// Swiper library
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  // Swiper Pagination
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+// ScrollReveal Library
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+  #home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+)
+
+/* Back-to-top Button */
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+// Menu Ativo conforme a sessão da página
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection() {
+  const checkPoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute(`id`)
+
+    const checkPointStart = checkPoint >= sectionTop
+    const checkPointEnd = checkPoint <= sectionTop + sectionHeight
+
+    if (checkPointStart && checkPointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+// When Scroll
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
